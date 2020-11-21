4dfff576b66ab86020e05478df66594336391146
sravanichinna <sravanidane413.com>
2020-11-21 14:16:46 +0530

    Added: SortingInDescendingOrder logic

--- ArithematicSorting.sh
+++ ArithematicSorting.sh
@@ -12,3 +12,17 @@ arr[1]=$result2
 arr[2]=$result3
 arr[3]=$result4
 echo -e ${arr[@]}
+temp=0
+for((i=0 ; i<=4 : i++ ))
+do
+	for((j=0 ; j<=4-i-1 ; j++ ))
+	do
+		if[[ ${arr[j]} -lt ${arr[$((j+1))]} ]]
+		then
+			temp=${arr[j]}
+			arr[j]=${arr[$((j+1))]}
+			arr[$((j+1))]=$temp
+		fi
+	done
+done
+echo "sort in decending order: " ${arr[@]}
